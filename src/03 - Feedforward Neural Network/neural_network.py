f7043a75c1169cf06cd6cd4b2fc235b2f4672364
Thach Jean-Pierre <jeanpierre.thach@gmail.com>
2018-05-26 17:43:54 -0400

    Added feedforward neural network

---
+++ src/03 - Feedforward Neural Network/neural_network.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+import torchvision
+
+import matplotlib.pyplot as plt
+
+import sys
+sys.path.append('..')
+from utils import get_mnist_dataset
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Hyperparameters
+input_size = 784
+hidden_size = 500
+num_classes = 10
+num_epochs = 10
+batch_size = 100
+learning_rate = 1e-3
+
+train_loader, test_loader = get_mnist_dataset(100)
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(NeuralNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, output_size)
+    
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
+
+model = NeuralNet(input_size, hidden_size, num_classes).to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+loss_history = []
+
+# Train the model
+total_step = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.reshape(-1, input_size).to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        loss_history.append(loss)
+        optimizer.step()
+
+        if (i+1) % 300 == 0:
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(
+                epoch+1, num_epochs, i+1, total_step, loss.item()))
+
+print('Final training loss: {:.4f}'.format(loss_history[-1].item()))
+
+# Test the model
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        images = images.reshape(-1, input_size).to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum()
+
+    print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
+
+# Plot the graph
+plt.plot(loss_history)
+plt.xlabel('iteration')
+plt.ylabel('training loss')
+plt.title('Training Loss history')
+plt.show()
+
+# Save the model checkpoint
+torch.save(model.state_dict(), 'model.ckpt')
